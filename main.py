a6dbdfde115569ff63b5ffab6539c600bddbda22
KGergo02 <kassa.gergo2002@gmail.com>
2024-04-05 00:17:54 +0200

    Commented API call, implemented cache instead. If you need new data, just uncomment the 2 lines

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from functions_for_animes import *
 
 anime_titles = get_anime_titles()
 
-anime_infos = get_new_data(anime_titles)
+# anime_infos = get_new_data_from_api(anime_titles)
+#
+# write_data_to_file(anime_infos)
+
+anime_infos = get_data_from_file()
 
 print(anime_infos)
