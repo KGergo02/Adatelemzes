7de64c12fbf26439cd56bc300d3c72dfdf1aa254
KGergo02 <kassa.gergo2002@gmail.com>
2024-04-05 00:14:25 +0200

    String override for custom objects

--- anime_info.py
+++ anime_info.py
@@ -3,3 +3,6 @@ class AnimeInfo:
         self.name = name
         self.rating = rating
         self.release_date = release_date
+
+    def __str__(self):
+        return f"{self.name} ({self.release_date}): {self.rating}"
