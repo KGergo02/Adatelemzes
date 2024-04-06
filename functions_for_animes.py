aeae63b576d44c1160a844246f38b79c70e3c00c
KGergo02 <kassa.gergo2002@gmail.com>
2024-04-06 16:12:18 +0200

    Moved plot creation to a function definition inside functions_for_animes.py

--- functions_for_animes.py
+++ functions_for_animes.py
@@ -3,7 +3,7 @@ import time
 import requests
 import xmltodict
 from anime_info import AnimeInfo
-
+import matplotlib.pyplot as plt
 
 def get_new_data_from_api(items):
     """
@@ -134,3 +134,31 @@ def get_data_from_file():
             animes.append(item)
 
     return animes
+
+
+def create_plot_rating(df):
+    df["release_date"] = pd.to_datetime(df["release_date"], errors="coerce")
+
+    covid_filter_df = df[df["release_date"].dt.year.between(2020, 2022)]
+    covid_filter_df.loc[:, "rating"] = pd.to_numeric(covid_filter_df["rating"], errors="coerce")
+    covid_avarage = covid_filter_df["rating"].mean()
+    print("COVID átlag:", covid_avarage)
+
+    pre_covid_filter_df = df[df["release_date"].dt.year.lt(2020)]
+    pre_covid_filter_df.loc[:, "rating"] = pd.to_numeric(pre_covid_filter_df["rating"], errors="coerce")
+    pre_covid_avarage = pre_covid_filter_df["rating"].mean()
+    print("Pre-COVID átlag:", pre_covid_avarage)
+
+    post_covid_filter_df = df[df["release_date"].dt.year.gt(2022)]
+    post_covid_filter_df.loc[:, "rating"] = pd.to_numeric(post_covid_filter_df["rating"], errors="coerce")
+    post_covid_avarage = post_covid_filter_df["rating"].mean()
+    print("Post-COVID átlag:", post_covid_avarage)
+
+    average_ratings = [pre_covid_avarage, covid_avarage, post_covid_avarage]
+    periods = ['Pre-COVID', 'COVID', 'Post-COVID']
+
+    plt.bar(periods, average_ratings, color=['blue', 'orange', 'green'])
+    plt.title('Átlagos értékelés COVID előtt, alatt és után')
+    plt.xlabel('Időszak')
+    plt.ylabel('Átlagos értékelés')
+    plt.show()
