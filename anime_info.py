91c366842cd962d9c6167621fe70b94d93c175cf
KGergo02 <kassa.gergo2002@gmail.com>
2024-04-12 19:15:11 +0200

    Expanded model for jsonification and added more properties

--- anime_info.py
+++ anime_info.py
@@ -1,8 +1,26 @@
 class AnimeInfo:
-    def __init__(self, name, rating, release_date):
+    def __init__(self, id, name, rating, release_date, news, genres, themes):
+        self.id = id
         self.name = name
         self.rating = rating
         self.release_date = release_date
+        self.news = news
+        self.genres = genres
+        self.themes = themes
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'rating': self.rating,
+            'release_date': self.release_date,
+            'news': self.news,
+            'genres': self.genres,
+            'themes': self.themes
+        }
+
+    def get_news_count(self):
+        return len(self.news)
 
     def __str__(self):
-        return f"{self.name} ({self.release_date}): {self.rating}"
+        return f"[{self.id}]{self.name} ({self.release_date}): {self.rating}, with {self.get_news_count()} appearances"
