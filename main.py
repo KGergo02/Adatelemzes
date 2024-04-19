331cb007d2cab8d027bbdee0df2d76128c8891d0
KGergo02 <kassa.gergo2002@gmail.com>
2024-04-19 17:25:14 +0200

    Sorting dataframe by max news_sum value

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from functions_for_animes import *
+import numpy as np
 
 if __name__ == '__main__':
 
-    ids = get_anime_ids()
-
-    #anime_infos = get_new_data_from_api(ids)
+    # ids = get_anime_ids()
+    #
+    # anime_infos = get_new_data_from_api(ids)
 
     # noinspection PyRedeclaration
     anime_infos = get_data_from_file()
 
     df = create_dataframe_from_model(anime_infos)
+
+    df = df.sort_values(by="news_sum", ascending=False)
+
+    print(df)
 
     # create_plot_rating(df)
