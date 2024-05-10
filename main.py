10a06550f96fdbda8e43430633bd8dbb6fcdaaad
Beeeruus <benak.kristian3@gmail.com>
2024-05-10 09:37:22 +0200

    added more analysis, and more plots

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from functions_for_animes import *
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 
 
 if __name__ == '__main__':
@@ -15,62 +16,103 @@ if __name__ == '__main__':
 
     df = df.sort_values(by="news_sum", ascending=False)
 
-    print(df)
+    #print(df)
 
+    
 
+    font = {'family': 'sans-serif',
+            'weight': 'bold',
+            'size': 12}
+
+    df['rating'] = pd.to_numeric(df['rating'], errors='coerce')
+    df.dropna(subset=['rating'], inplace=True)
+    df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
+    df.dropna(subset=['release_date'], inplace=True)
+
+    
+    
+    
+    """""
+    df['anime_age'] = 2024 - df['release_date'].dt.year
+    
+    plt.rc('font', **font) 
+    plt.figure(figsize=(8, 12),)
+    plt.scatter(df['anime_age'], df['news_sum'], color='red', edgecolors='red', linewidths=5, alpha=0.7)
+    plt.title('ANIME KORÁNAK ÉS A KAPOTT HÍREK SZÁMÁNAK ÖSSZEFÜGGÉSE', fontweight = 'bold', fontsize = 15)
+    plt.xlabel('ANIME KORA', fontweight = 'bold')
+    plt.ylabel('HÍREK SZÁMA', fontweight = 'bold')
+    plt.grid(True)
+    plt.show()
+    """""
+
+    
+
+    
+    """""
+    df['rating'] = pd.to_numeric(df['rating'], errors='coerce')
+    top_10_rating = df.nlargest(10, 'rating')
+
+    plt.rc('font', **font) 
+    plt.figure(figsize=(10, 10))
+    plt.bar(top_10_rating['name'], top_10_rating['rating'], color='purple', edgecolor='black', linewidth=1)
+    plt.title('TOP 10 LEGJOBB ÉRTÉKELÉSŰ ANIMÉK', fontweight = 'bold', fontsize = 15)
+    
+    plt.xticks(rotation=45, ha='right') 
+    plt.yticks(range(1, 11)) 
+    plt.grid(False)
+    plt.tight_layout()
+    plt.show()
+    """""
+    
+    
+    
+    
+    top_10_news = df.nlargest(10, 'news_sum')
+    
+    plt.rc('font', **font)
+    plt.figure(figsize=(10, 12))
+    plt.bar(top_10_news['name'], top_10_news['news_sum'], color='skyblue',  edgecolor='black', linewidth=1)
+    
+    plt.ylabel('Hírek száma', fontweight = 'bold')
+    plt.title('TOP 10 ANIME A LEGTÖBB HÍRREL', fontweight = 'bold', fontsize = 15)
+    plt.xticks(rotation=45, ha='right', fontweight = 'bold')
+    plt.tight_layout()
+    plt.show()
     
     df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
     df['year'] = df['release_date'].dt.year
     news_per_year = df.groupby('year')['news_sum'].sum()
+    
 
+    
     df.dropna(inplace=True)
     highest_rated_anime = df[df['rating'] == df['rating'].max()]
     
-
+    
+    """""
+    df['year'] = df['release_date'].dt.year
+    df_10_years = df.groupby('year')['news_sum'].sum().tail(10)
+    
     plt.figure(figsize=(10, 6))
-    news_per_year.plot(kind='line', marker='o')
-    plt.title('Évenkénti hírek száma')
-    plt.xlabel('Év')
-    plt.ylabel('Hírek száma')
-    plt.grid(True)
-    plt.show()	
-    
-
-
-    
-
-
-
-    
-    df.sort_values(by='release_date', inplace=True)
-
-    plt.figure(figsize=(10, 6))
-    plt.plot(df['release_date'], df['rating'], marker='o', label='Értékelés')
-    plt.plot(df['release_date'], df['news_sum'], marker='o', label='Hírek száma')
-    plt.title('Animek értékelése és hírek száma az idő múlásával')
-    plt.xlabel('Megjelenés dátuma')
-    plt.ylabel('Értékelés / Hírek száma')
-    plt.legend()
-    plt.grid(True)
-    plt.xticks(rotation=45) 
-    plt.tight_layout()
+    df_10_years.plot(kind='bar', color='green', edgecolor='black', linewidth=1)
+    plt.title('HÍREK SZÁMA AZ ELMÚLT 10 ÉVBEN', fontweight = 'bold', fontsize = 15)
+    plt.grid(False)
     plt.show()
+    """""
     
+    
+   
+
+   
+
+
+    
+    
+   
     
      
-    df['anime_age'] = 2024 - df['release_date'].dt.year
-
-    plt.figure(figsize=(10, 6))
-    plt.scatter(df['anime_age'], df['news_sum'], color='b', alpha=0.5)
-    plt.title('Anime korának és kapott hírek számának összefüggése')
-    plt.xlabel('Anime kora (év)')
-    plt.ylabel('Kapott hírek száma')
-    plt.grid(True)
-
-    plt.tight_layout()
-    plt.show()  
-
+      
+ 
     
-
     
     # create_plot_rating(df)
